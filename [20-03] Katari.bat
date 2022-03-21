835d0d9ea2151537f5f776149b825f53d855a654
Saguma <77213074+gsaguma@users.noreply.github.com>
2022-03-20 23:42:03 -0500

    Update [20-03] Katari.bat

--- [20-03] Katari.bat
+++ [20-03] Katari.bat
@@ -1,7 +1,7 @@
 @echo off
-title Katari v1.0
+title Katari v1.1
 color e
-mode 70,30
+mode 75,30
 
 :inicio
 echo.
@@ -16,9 +16,9 @@ echo        ********************************************
 echo        *  Bienvenido. Gracias por usar mi codigo  *
 echo        ********************************************
 echo              [1] Crackear red wifi
-echo              [2] Cambiar clave de pc
-echo              [3] Contactar al creador
-echo              [4] Salir                         
+echo              [2] Crackear clave de pc
+echo              [3] Info sobre el script
+echo              [5] Salir                         
 echo.
 echo.
 set /p menu=Opcion: 
@@ -29,6 +29,7 @@ if "%menu%"=="4" goto salir
 
 :op1
 echo.
+echo Nota:La red se debe haber usado al menos una vez en este dispositivo
 set /p red=Nombre de la Red: 
 netsh wlan show profile name="%red%" key=clear>apuntes.txt
 echo  [*] Clave guardada.
@@ -38,6 +39,7 @@ goto inicio
 
 :op2
 echo.
+echo Nota: Usuario es el nombre de la pc
 set /p usuario=Usuario: 
 net user "%usuario%" *
 pause>nul
@@ -46,10 +48,15 @@ goto inicio
 
 :op3
 echo.
-echo        +++++++++++++++++++++++++++++
-echo        + Github:     gsaguma       +
-echo        + Discord:   saguma#7066    +
-echo        +++++++++++++++++++++++++++++
+echo        +++++++++++++++++++++++++++++++
+echo        +  Github:     gsaguma        +
+echo        +  Discord:   saguma#7066     +
+echo        +++++++++++++++++++++++++++++++
+echo.
+echo.
+echo         |     CAMBIOS REALIZADOS     |
+echo         |-> Notas de guía en comandos|
+echo         |-> Agregado apartado de info|
 pause>nul
 cls
 goto inicio
