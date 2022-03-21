413d07d40ed8a5d0f30229345fea28426a20ae7f
Saguma <77213074+gsaguma@users.noreply.github.com>
2022-03-21 15:52:10 -0500

    Update [20-03] Katari.bat

--- [20-03] Katari.bat
+++ [20-03] Katari.bat
@@ -1,7 +1,7 @@
 @echo off
 title Katari v1.1
 color e
-mode 75,30
+mode 65,30
 
 :inicio
 echo.
@@ -28,18 +28,28 @@ if "%menu%"=="3" goto op3
 if "%menu%"=="4" goto salir
 
 :op1
+cls
+netsh wlan show profiles
 echo.
-echo Nota:La red se debe haber usado al menos una vez en este dispositivo
+echo              --------------------------------
+echo              - Estas redes pueden hackearse - 
+echo              --------------------------------
+echo.
 set /p red=Nombre de la Red: 
 netsh wlan show profile name="%red%" key=clear>apuntes.txt
+echo.
 echo  [*] Clave guardada. Revisa el archivo apuntes en el escritorio.
 pause>nul
 cls
 goto inicio
 
 :op2
+cls
+net user
 echo.
-echo Nota: Usuario es el nombre de la pc
+echo             ------------------------------
+echo             - Escribe el primer usuario  -
+echo             ------------------------------
 set /p usuario=Usuario: 
 net user "%usuario%" *
 pause>nul
@@ -47,18 +57,28 @@ cls
 goto inicio
 
 :op3
+cls
 echo.
-echo        +++++++++++++++++++++++++++++++
-echo        +  Github:     gsaguma        +
-echo        +  Discord:   saguma#7066     +
-echo        +++++++++++++++++++++++++++++++
+echo   __  __     ______     ______   ______     ______     __    
+echo  /\ \/ /    /\  __ \   /\__  _\ /\  __ \   /\  == \   /\ \  
+echo  \ \  _"-.  \ \ \__\\  \/_/\ \/ \ \ \__\\  \ \  __<   \ \ \ 
+echo   \ \_\ \_\  \ \_\ \_\    \ \_\  \ \_\ \_\  \ \_\ \_\  \ \_\ 
+echo    \/_/\/_/   \/_/\/_/     \/_/   \/_/\/_/   \/_/ /_/   \/_/ 
+echo                                                    -by Saguma
 echo.
 echo.
-echo        ===============================
-echo        =      CAMBIOS REALIZADOS     =
-echo        = Notas de guia en comandos   =
-echo        = Agregado apartado de info   =
-echo        ===============================
+echo                 +++++++++++++++++++++++++++++++
+echo                 +  Github:     gsaguma        +
+echo                 +  Discord:   saguma#7066     +
+echo                 +++++++++++++++++++++++++++++++
+echo.
+echo.
+echo.
+echo                 ===============================
+echo                 =      CAMBIOS REALIZADOS     =
+echo                 = Notas de guia en comandos   =
+echo                 = Agregado apartado de info   =
+echo                 ===============================
 pause>nul
 cls
 goto inicio
